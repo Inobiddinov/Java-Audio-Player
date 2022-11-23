8747da2a0ea6765e7586001d2dab9e59f0969452
Mukhammad aziz <87309688+Inobiddinov@users.noreply.github.com>
2022-11-23 22:12:55 +0800

    Add files via upload
    My audio file is 40 mb and github can hold only 25 MB so it is not possible to upload here you can put your song and change the file name.

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import javax.crypto.spec.PSource;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+       File file = new File("111.wav");
+       Scanner scan = new Scanner(System.in);
+        AudioInputStream audio = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audio);
+
+
+        String response = "";
+        while (!response.equals("Q")){
+            System.out.println("P = play, S = stop, R= reset, Q = quit.");
+            System.out.println("Enter your Choice here:");
+            response = scan.next();
+            response = response.toUpperCase();
+
+            switch (response){
+                case "P" : clip.start();
+                break;
+                case "S" : clip.stop();
+                break;
+                case "R" : clip.setMicrosecondPosition(0);
+                break;
+                case "Q" : clip.close();
+                    System.out.println("Bye See you soon");
+                break;
+                default :
+                    System.out.println("not valid value Can you try it again:");
+            }
+        }
+        clip.start();
+    }
+}
